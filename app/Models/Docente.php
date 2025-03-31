2909f15c04bcdaf5b5ee0b9c63128422043f8388
Samuel Mercado <samuel.mg@gmx.com>
2025-03-31 15:47:31 -0600

    Crea modelo Docente con opción -a
    - Crea modelo, migración, controladore, factory, seeder y requests
    - Configura fillable en modelo
    - Crea tabla mediante migración
    - Configura no uso de timestamps

---
+++ app/Models/Docente.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Docente extends Model
+{
+    /** @use HasFactory<\Database\Factories\DocenteFactory> */
+    use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'nombre',
+        'correo',
+        'codigo',
+    ];
+}
