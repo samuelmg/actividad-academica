fe926199d1c134de329f2290ebbd0f97d5f9848c
Samuel Mercado <samuel.mg@gmx.com>
2025-04-02 15:55:29 -0600

    Deshabilita pluralización en español

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Pluralizer::useLanguage('spanish');
+        // Pluralizer::useLanguage('spanish');
     }
 }
