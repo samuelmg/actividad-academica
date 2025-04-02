8c25d9ceb9b19f64350c58282b9c2865d96b910f
Samuel Mercado <samuel.mg@gmx.com>
2025-04-02 16:30:59 -0600

    Crea listado de alumnos con sus secciones

--- app/Http/Controllers/AlumnoController.php
+++ app/Http/Controllers/AlumnoController.php
@@ -15,7 +15,9 @@ class AlumnoController extends Controller
      */
     public function index()
     {
-        //
+        return view('alumnos.alumno-index', [
+            'alumnos' => Alumno::all(),
+        ]);
     }
 
     /**
---
+++ resources/views/alumnos/alumno-index.blade.php
@@ -0,0 +1,27 @@
+<x-layouts.app :title="__('Info Alumno')">
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+            
+            <table border="1">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Correo</th>
+                    <th>Secciones</th>
+                </tr>
+
+                @foreach ($alumnos as $alumno)
+                    <tr>
+                        <td><a href="{{ route('alumno.show', $alumno) }}">{{ $alumno->nombre }}</a></td>
+                        <td>{{ $alumno->correo }}</td>
+                        <td>
+                            @foreach ($alumno->secciones as $seccion)
+                                {{ $seccion->nombre }} - {{ $seccion->seccion }}<br>
+                            @endforeach
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+
+        </div>
+    </div>
+</x-layouts.app>
